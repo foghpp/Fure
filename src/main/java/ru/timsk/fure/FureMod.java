f1255d7f28d2228fb0826041557ca78b93149300
foghpp <61115231+foghpp@users.noreply.github.com>
2022-06-10 17:17:50 +0500

    Add Hunger charm

--- src/main/java/ru/timsk/fure/FureMod.java
+++ src/main/java/ru/timsk/fure/FureMod.java
@@ -1,19 +1,27 @@
 package ru.timsk.fure;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import ru.timsk.fure.charm.HungerCharm;
 
 public class FureMod implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
+
 	public static final Logger LOGGER = LoggerFactory.getLogger("fure");
+
+	public static final HungerCharm HUNGER_CHARM = new HungerCharm();
 
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
+
+		Registry.register(Registry.ITEM, new Identifier("fure", "hunger_charm"), HUNGER_CHARM);
 	}
 }
---
+++ src/main/java/ru/timsk/fure/charm/HungerCharm.java
@@ -0,0 +1,26 @@
+package ru.timsk.fure.charm;
+
+import com.google.common.collect.Multimap;
+import dev.emi.trinkets.api.SlotReference;
+import dev.emi.trinkets.api.TrinketItem;
+import java.util.UUID;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.attribute.EntityAttribute;
+import net.minecraft.entity.attribute.EntityAttributeModifier;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+
+public class HungerCharm extends TrinketItem {
+    public HungerCharm() {
+        super(new FabricItemSettings().group(ItemGroup.COMBAT).maxCount(1));
+    }
+
+    public Multimap<EntityAttribute, EntityAttributeModifier> getModifiers(ItemStack stack, SlotReference slot, LivingEntity entity, UUID uuid) {
+        Multimap<EntityAttribute, EntityAttributeModifier> modifiers = super.getModifiers(stack, slot, entity, uuid);
+
+        // Implementation goes here.
+
+        return modifiers;
+    }
+}
